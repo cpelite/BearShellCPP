5d76f3d4d71f30a6c77f01ae1e9500274de6492b
CPElite / Lynn F <cpelite@zlatina.tech>
2023-04-16 21:10:54 +0200

    few changes

--- BearShellCPP.cpp
+++ BearShellCPP.cpp
@@ -1,51 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
-void info() {
-    std::cout << "BearShellCPP v0.0.1";
-    std::cout << "Copyright B. Fellner / @CPElite (github.com)";
-}
-
-void echo() {
-    std::string echo;
-    std::cout << "Please enter something to get it echoed back: ";
-    std::cin >> echo;
-    std::cout << echo << "\n";
-}
-
-void commlist() {
-    std::cout << "The following commands are available: info - displays information about the shell. \necho - echoes user entry back. \ncommlist - shows this list. \nrfile - reads from a user specified file. \nlyrica - reads from lyrica.txt.";
-}
-
-void clearscreen() {
-    system("clear");
-}
-
-void readfile() {
-    std::string mytext;
-    std::string filepath;
-    std::cout << "Please enter the filename:";
-    std::cin >> filepath; 
-    std::fstream FileToRead(filepath);
-    while (getline (FileToRead, mytext)) {
-        std::cout << mytext;
-        //FileToRead.close();
-    }
-    FileToRead.close();
-    std::cout << "\n";
-
-}
-
-void lyrica() {
-    std::fstream FileToRead("lyrica.txt");
-    char ch;
-    while ( FileToRead ) {
-        FileToRead.get(ch);
-        std::cout << ch;
-    }
-    std::cout << std::endl;
-}
+#include "comms.cpp"
 
 int main(int, char**)
 {
@@ -83,9 +39,15 @@ int main(int, char**)
         else if (input == "lyrica") {
             lyrica();
         }
+        else if (input == "mkfile") {
+            mkfile();
+        }
+        else if (input == "filemod") {
+            filemod();
+        }
         else
         {
-            std::cout << "Unrecognized command. Please type help for a list of commands." << std::endl;
+            std::cout << "Unrecognized command. Please type commlist for a list of commands." << std::endl;
         }
     }
 
